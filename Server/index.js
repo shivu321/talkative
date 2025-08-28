040146b47164b4bdd92ecd6a8f92cd93af077870
Shivam Mishra <shivammishra0987@gmail.com>
2025-08-29 01:06:08 +0530

    backend fixed also

--- Server/index.js
+++ Server/index.js
@@ -27,7 +27,7 @@ const io = new Server(httpServer, {
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "*",
         credentials: true,
     })
 );
