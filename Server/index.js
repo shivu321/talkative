f550025eefd583fb7c45cec43e5e4ac11a60f8c7
Shivam Mishra <Shivammishra0987@gmail.com>
2025-08-29 10:19:26 +0530

    fixed chanegs

--- Server/index.js
+++ Server/index.js
@@ -19,7 +19,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "*"
+        origin: "https://api.talkative.co.in"
     },
 });
 
